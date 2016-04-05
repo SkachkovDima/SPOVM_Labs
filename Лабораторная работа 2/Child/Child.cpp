3847661062a7912f2e6bd298dec2529afaa6eab4
Dima Skachkov <Dima Skachkov>
2016-04-05 10:31:09 +0300

    lab 2 ver.3

--- Лабораторная работа 2/Child/Child.cpp
+++ Лабораторная работа 2/Child/Child.cpp
@@ -1,40 +1,4 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef _MSC_VER
-#include <Windows.h>
-
-#else
-#include <termios.h>
-#include <time.h>
-#include <sys/wait.h>
-#include <list>
-#include <sys/types.h>	
-#include <unistd.h>
-#include <dirent.h>
-#include <cstring>
-#include <string>
-#include <signal.h>
-
-bool closeFlag = true;
-bool activateFlag = false;
-
-void fActivate(int){
-	signal(SIGUSR1, fActivate);
-	activateFlag = true;
-}
-
-void fClose(int){
-	signal(SIGUSR2, fClose);
-	closeFlag = false;
-}
-
-#endif
-
-using namespace std;
-
-#define SIZE 100
+#include "head.h"
 
 int main(int argc, char* argv[]){
 	setlocale(LC_ALL, "Russian");
